984a329352c05a01e158bd28ba3ee60ea2812302
Anna <agalucnko@gmail.com>
2022-11-12 20:59:02 +0200

    add changes

--- js/task-08.js
+++ js/task-08.js
@@ -12,6 +12,7 @@ function formElSubmitHendler(evt){
 
     if (email.value === '' || password.value === ''){
         alert ("Please fill in all the fields!");
+        return  evt.currentTarget.reset();
     }
       
     console.log(`Login: ${email.value}, Password: ${password.value}`);
